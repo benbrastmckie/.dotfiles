eb72f4a770fffe44961bf73f14a6455c12e750e0
benbrastmckie <benbrastmckie@gmail.com>
2024-02-20 12:04:23 -0500

    init commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "system config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # home-manager = {
+    #   url = "github:nix-community/home-manager/release-23.11";  
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+    username = "benjamin";
+    name = "Ben";
+  in {
+    nixosConfigurations = {
+      nandi = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+        specialArgs = {
+          inherit username;
+          inherit name;
+          inherit pkgs-unstable;
+        };
+      };
+    };
+    homeConfigurations = {
+      benjamin = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+        extraSpecialArgs = {
+          inherit username;
+          inherit name;
+          inherit pkgs-unstable;
+        };
+      };
+    };
+  };
+}
