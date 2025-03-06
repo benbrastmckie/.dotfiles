c879113c3e88aea682ab6fb9ad9ee543cc081576
Benjamin Brast-McKie <benbrastmckie@gmail.com>
2025-03-06 01:48:02 -0500

    added note

--- home.nix
+++ home.nix
@@ -45,11 +45,12 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".config/neofetch/config.conf".source = ./config/neofetch.conf;
+    ".config/niri/config.kdl".source = ./config/config.kdl;
+    # NOTE: The following are excluded since they belong to .config for others to use
     # ".config/fish/config.fish".source = ./config/config.fish;
     # ".config/kitty/kitty.conf".source = ./config/kitty.conf;
     # ".config/zathura/zathurarc".source = ./config/zathurarc;
-    ".config/neofetch/config.conf".source = ./config/neofetch.conf;
-    ".config/niri/config.kdl".source = ./config/config.kdl;
     # ".config/alacritty/alacritty.toml".source = ./config/alacritty.toml;
     # ".tmux.conf".source = ./config/.tmux.conf;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
